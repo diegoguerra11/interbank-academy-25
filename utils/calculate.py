6ab5b474e2b6770710d9fed23a731cb1882cafc1
Diego Guerra <guerradiego92@gmail.com>
2025-04-20 13:18:54 -0500

    NEW VALIDATIONS AND EXCEPTIONS THROWN IN FUNCTIONS ADDED

--- utils/calculate.py
+++ utils/calculate.py
@@ -3,12 +3,12 @@ from pandas import DataFrame
 
 from utils.validations import is_str, is_numeric, df_missing_columns, serie_missing_columns
 
-def add_by_column(df: DataFrame, column: str, categories: list[str]) -> float:
+def add_by_column(df: DataFrame, categorical_column: str, categories: list[str]) -> float:
     """ Allows the addition of the values ​​of a column according to the type 
            
         Args:
             df (pandas.Dataframe): dataframe
-            column (str): column to which the addition will be applied
+            categorical_column (str): column to which the addition will be applied
             categories(list[str]): categories on which the addition will be performed
 
         Returns:
@@ -17,28 +17,30 @@ def add_by_column(df: DataFrame, column: str, categories: list[str]) -> float:
         :raises: Exception: If any exception occurs, the message is returned and the -9999999 value is returned.            
     """
     try:
-        missing = df_missing_columns(df, [column])
-        if(len(missing) > 0): raise Exception(f"The following columns do not exist in the dataframe: ", missing)
-    
-        type_mission = serie_missing_columns(df[column], categories)
-        if(len(type_mission) > 0): raise Exception(f"The following categories do not exist in the dataframe: ", type_mission)
+        columns_missings = df_missing_columns(df, [categorical_column])
+        if(len(columns_missings) > 0): raise Exception(f"The following columns do not exist in the dataframe: ", columns_missings)
+
+        if(not is_str(df, categorical_column)): raise Exception("only category column")
+
+        categories_missings = serie_missing_columns(df[categorical_column], categories)
+        if(len(categories_missings) > 0): raise Exception(f"The following categories do not exist in the dataframe: ", categories_missings)
 
         sum = 0
         
         for category in categories: 
-            sum += df[column][category]
+            sum += df[categorical_column][category]
 
         return sum
     except Exception as e:
         print(f"add_by_column: {e}")
         return -9999999
 
-def sub_by_column(df: DataFrame, column: str, categories: list[str]) -> float:
+def sub_by_column(df: DataFrame, categorical_column: str, categories: list[str]) -> float:
     """ Allows the subtraction of the values ​​of a column according to the category 
         
         Args:
             df (pandas.Dataframe): dataframe
-            column (str): column to which the subtraction will be applied
+            categorical_column (str): column to which the subtraction will be applied
             categories(list[str]): categories on which the subtraction will be performed
 
         Returns:
@@ -49,18 +51,20 @@ def sub_by_column(df: DataFrame, column: str, categories: list[str]) -> float:
     try:
         sub = 0
 
-        missing = df_missing_columns(df, [column])
-        if(len(missing) > 0): raise Exception(f"The following columns do not exist in the dataframe: ", missing)
+        columns_missings = df_missing_columns(df, [categorical_column])
+        if(len(columns_missings) > 0): raise Exception(f"The following columns do not exist in the dataframe: ", columns_missings)
 
-        type_mission = serie_missing_columns(df[column], categories)
-        if(len(type_mission) > 0): raise Exception(f"The following categories do not exist in the dataframe: ", type_mission)
+        if(not is_str(df, categorical_column)): raise Exception("only category column")
+
+        categories_missings = serie_missing_columns(df[categorical_column], categories)
+        if(len(categories_missings) > 0): raise Exception(f"The following categories do not exist in the dataframe: ", categories_missings)
         
         for category in categories:
             if(sub == 0): 
-                sub = df[column][category]
+                sub = df[categorical_column][category]
                 continue
 
-            sub -= df[column][category]
+            sub -= df[categorical_column][category]
 
         return sub
     except Exception as e:
@@ -69,9 +73,9 @@ def sub_by_column(df: DataFrame, column: str, categories: list[str]) -> float:
 
 def calculate_by_grouped(
     df: DataFrame,
-    group_by: str,
+    grouping_column: str,
     operation: Literal["add", "sub"],
-    column: str,
+    categorical_column: str,
     categories: list[str]
 ) -> float:
     """ Allows you to group based on one column, 
@@ -90,20 +94,20 @@ def calculate_by_grouped(
         :raises: Exception: If any exception occurs, the message is returned and the -9999999 value is returned.              
     """
     try:
-        missing = df_missing_columns(df, [group_by, column])
+        columns_missings = df_missing_columns(df, [grouping_column, categorical_column])
 
-        if(len(missing) > 0): raise Exception(f"The following columns do not exist in the dataframe: ", missing)
-        if(not is_str(df, group_by)): raise Exception("Group by categorical column only")
-        if(not is_numeric(df, column)):  raise Exception("Only operation by column numeric")
+        if(len(columns_missings) > 0): raise Exception(f"The following columns do not exist in the dataframe: ", columns_missings)
+        if(not is_str(df, grouping_column)): raise Exception("Group by categorical column only")
+        if(not is_numeric(df, categorical_column)):  raise Exception("Only operation by column numeric")
 
         # group by the desired column and sum by column
-        sum_by_type = df.groupby(group_by).sum()
+        sum_by_type = df.groupby(grouping_column).sum()
 
         result = 0
 
         match operation:
-            case "sum": result = add_by_column(sum_by_type, column, categories)
-            case "sub": result = sub_by_column(sum_by_type, column, categories)
+            case "sum": result = add_by_column(sum_by_type, categorical_column, categories)
+            case "sub": result = sub_by_column(sum_by_type, categorical_column, categories)
             case _: raise Exception("Only add or sub operations can be performed")
 
         return round(result, 2)
@@ -124,9 +128,9 @@ def count_by_column(df: DataFrame, column: str) -> dict:
         :raises: Exception: If any exception occurs, the message is returned and an empty dict is returned.
     """
     try:
-        missing = df_missing_columns(df, [column])
+        columns_missings = df_missing_columns(df, [column])
         
-        if(len(missing) > 0): raise Exception(f"The following columns do not exist in the dataframe: ", missing)
+        if(len(columns_missings) > 0): raise Exception(f"The following columns do not exist in the dataframe: ", columns_missings)
 
         return df[column].value_counts().to_dict()
     except Exception as e:
